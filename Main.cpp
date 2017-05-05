a91e21c38ca9898b89d3e63998482230a1e47f09
OscarProgra3 <Daniel.embu96nitec.edu>
2017-05-05 14:42:25 -0600

    yolo

--- Main.cpp
+++ Main.cpp
@@ -16,26 +16,44 @@ int main()
 		if (opc==1)
 		{
 			
-			int num;
+			string numerosmalos="";
+			int num,n;
 		    string cad = "";
-		    cout<<"Ingrese el numero\n";
-		    cin >> num;
-		    if(num > 0){
-		        while(num>0){
-		           if(num%2 == 0)
-		                cad = "0" + cad;
-		            else
-		                cad = "1" + cad;
-		                num = num/2;
-		        }
-		        cout <<"Numero en binario: " <<cad;
-		    }
-		    else{
-		        if(num == 0)
-		            cout << "0";
-		        else
-		            cout << "Solo numeros mayores a cero";
-		    }
+		    cout<<"Ingrese hasta que numero el numero\n";
+		    cin >> n;
+		    for (int i = 0; i < n; ++i)
+		    {
+		    	int cont=0;
+		    	num=i;
+			    if(num > 0){
+			        while(num>0){
+			           if(num%2 == 0)
+			                cad = "0" + cad;
+			            else
+			                cad = "1" + cad;
+			                num = num/2;
+			                cont++;
+			        }
+			        //cout <<"Numero en binario: " <<cad;
+			    }
+			    /*else{
+			        if(num == 0)
+			            cout << "0";
+			        //else
+			          //  cout << "Solo numeros mayores a cero";
+			    }*/
+			    if (cont%2==0)
+			    {
+			    	stringstream ss;
+			    	ss<<i;
+			    	string str = ss.str(); 
+
+			    	numerosmalos+=str+",";
+			    }
+			} 
+
+			cout<<endl<<"Los numeros malvados son : "<<numerosmalos<<endl;
+
 		}
 		//---------------------------------------------------------------------
 		if (opc==2)
